74ee75747ae2046695b239489c7983b36be3d935
Prerit <113082241+Prerit-nannu@users.noreply.github.com>
2023-04-02 17:50:48 +0530

    Update settings.py

--- E_Commerse/settings.py
+++ E_Commerse/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-+_5yzrjxgiwn^fex0)2#ptw6#t2hladp5ankc$6gx9d-#@q!tn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '13.234.203.29']
+ALLOWED_HOSTS = ['localhost', '13.234.203.29', '43.205.31.181']
 
 
 # Application definition
